f080f60db7ede522cb98144a9bb7bc86a5d3bcc1
Alberto Carvalho <alberto.carvalho@faro.com>
2023-03-06 19:30:43 +0000

    Creating Controller and Items entity

---
+++ AspNetCore/Learning01/Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers   
+{
+    [ApiController] //this brings default behaviors to this class that makes our life easier
+    //[Route("[controller]")] //define the route to which this controller will responding. This means GET /items
+    [Route("items")] //define the route to which this controller will responding. This means GET /items
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            
+            return items;
+        }
+
+        [HttpGet("{id}")] // GET /items/{id}
+        public Item GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+
+            return item;
+        }
+    }
+}
