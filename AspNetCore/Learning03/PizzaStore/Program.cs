add0aa9894be7f406f7bcb66806dac1a813401a3
Alberto Carvalho <alberto.carvalho@faro.com>
2023-03-07 18:25:38 +0000

    Learning03 - pizzas post endpoint to create new pizza

--- AspNetCore/Learning03/PizzaStore/Program.cs
+++ AspNetCore/Learning03/PizzaStore/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using PizzaStore.Data;
+using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 //Regist some services
@@ -27,5 +28,12 @@ app.UseSwaggerUI(c =>
 //Configure routes
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/pizzas", async (PizzaContext db) => await db.Pizzas.ToListAsync());
+
+app.MapPost("/pizza", async (PizzaContext db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
 
 app.Run();
