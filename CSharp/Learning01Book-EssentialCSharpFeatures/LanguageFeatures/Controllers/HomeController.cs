185c49cfa38926e7ebfc469d226dba4e22e6049f
Alberto Carvalho <alberto.carvalho@faro.com>
2023-04-07 18:27:09 +0100

    CSharp - Learning01 - Essential Features - CSharp - Learning01 - Essential Features - Adding controller and view

---
+++ CSharp/Learning01Book-EssentialCSharpFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() {
+
+            //We provide to the view an array of string's
+            return View(new string[] { "C#", "Language", "Features" });
+        }
+    }
+}
