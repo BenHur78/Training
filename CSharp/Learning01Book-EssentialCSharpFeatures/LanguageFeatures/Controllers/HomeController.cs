3e6d09112632e1541853a632dcf64e3e9460b9a2
Alberto Carvalho <alberto.carvalho@faro.com>
2023-04-09 11:23:10 +0100

    CSharp - Learning01 - Essential Features - CSharp - Learning01 - Essential Features - the single statement pattern of selecting the data to display in the view

--- CSharp/Learning01Book-EssentialCSharpFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ CSharp/Learning01Book-EssentialCSharpFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -2,34 +2,9 @@
 {
     public class HomeController : Controller
     {
-
-        //bool FilterByPrice(Product? p)
-        //{
-        //    return (p?.Price ?? 0) >= 20;
-        //}
-
         public ViewResult Index() {
 
-            ShoppingCart cart = new () { Products = Product.GetProducts() };
-
-            Product[] productArray = {
-                new Product { Name = "Kayak", Price = 275M},
-                new Product { Name = "Lifejacked", Price = 48.95M},
-                new Product { Name = "Soccer Ball", Price = 19.50M},
-                new Product { Name = "Corner flag", Price = 34.95M},
-            };
-
-            //Func<Product?, bool> nameFilter = delegate (Product? product) 
-            //{
-            //    return product?.Name?[0] == 'S';
-            //};
-
-            decimal priceFilterTotal = productArray.Filter(p => (p?.Price ?? 0) >= 20).TotalPrices();
-            decimal nameFilterTotal = productArray.Filter(p => p?.Name?[0] == 'S').TotalPrices();
-
-            return View("Index", new string[] { 
-                $"Price Total: {priceFilterTotal:C2}", 
-                $"Name Total: {nameFilterTotal:C2}" });
+            return View(Product.GetProducts().Select(p => p?.Name));
         }
     }
 }
