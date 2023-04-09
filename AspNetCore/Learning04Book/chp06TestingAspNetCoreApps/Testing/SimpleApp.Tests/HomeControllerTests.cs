75650b7e81ad165b7ae6c0c3399944d6e7f0de2d
Alberto Carvalho <alberto.carvalho@faro.com>
2023-04-09 20:03:51 +0100

    Learning04Book - chapter 06 - testing the HomeController

---
+++ AspNetCore/Learning04Book/chp06TestingAspNetCoreApps/Testing/SimpleApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleApp.Controllers;
+using SimpleApp.Models;
+
+namespace SimpleApp.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexActionModelIsComplete()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            Product[] products = new Product[] {
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M}
+            };
+
+            // Act
+            var viewResult = controller.Index() as ViewResult;
+            var model = viewResult?.ViewData.Model as IEnumerable<Product>;
+
+            // Assert
+            Assert.Equal(products, model, Comparer.Get<Product>( (p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
+        }
+    }
+}
