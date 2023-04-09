007f435d7ee1c544fe24530a4904400f7599898b
Alberto Carvalho <alberto.carvalho@faro.com>
2023-04-09 13:12:28 +0100

    CSharp - Learning01 - Essential Features - CSharp - Learning01 - Essential Features - setup the use case that will explain the problem that asynchronous enumerables solves.

--- CSharp/Learning01Book-EssentialCSharpFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ CSharp/Learning01Book-EssentialCSharpFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -4,9 +4,14 @@
     {
         public async Task<ViewResult> Index()
         {
-            long? length = await MyAsincMethods.GetPageLength();
-            
-            return View(new string[] { $"Length: {length}" });
+            List<string> output = new List<string>();
+
+            foreach(long? len in await MyAsincMethods.GetPageLengths(output, "apress.com", "microsoft.com", "amazon.com"))
+            {
+                output.Add($"Page length: { len }");
+            }
+
+            return View(output);
         }
     }
 }
