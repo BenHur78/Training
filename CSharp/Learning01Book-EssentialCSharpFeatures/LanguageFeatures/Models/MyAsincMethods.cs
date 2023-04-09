7930d619a257efba5f5b174f93d9420a59d28249
Alberto Carvalho <alberto.carvalho@faro.com>
2023-04-09 12:36:26 +0100

    CSharp - Learning01 - Essential Features - CSharp - Learning01 - Essential Features - setup the problem that async/await keywords try to solve.

---
+++ CSharp/Learning01Book-EssentialCSharpFeatures/LanguageFeatures/Models/MyAsincMethods.cs
@@ -0,0 +1,13 @@
+﻿namespace LanguageFeatures.Models
+{
+    public class MyAsincMethods
+    {
+        public static Task<long?> GetPageLength()
+        {
+            HttpClient client= new HttpClient();
+            var httpTask = client.GetAsync("http://apress.com");
+
+            return httpTask.ContinueWith( (Task<HttpResponseMessage> antecedent) => { return antecedent.Result.Content.Headers.ContentLength; } );
+        }
+    }
+}
