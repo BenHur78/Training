22e6247b7487ea6e0c3b71031f034ff1c4f1dbb9
Alberto Carvalho <alberto.a.carvalho@gmail.com>
2023-10-05 18:37:59 +0100

    Html - Dealing with Unassigned and Null Values I

--- Angular/Learning01Book/chp03/Primer/src/main.ts
+++ Angular/Learning01Book/chp03/Primer/src/main.ts
@@ -15,3 +15,11 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   let condition: boolean = true;
   let person: string = "Bob";
   const age: number = 40;
+
+  let place;
+console.log("Place value: " + place + " Type: " + typeof(place));
+place = "London";
+console.log("Place value: " + place + " Type: " + typeof(place));
+
+place = null;
+console.log("Place value: " + place + " Type: " + typeof(place));
