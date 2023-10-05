fc0d48cb498d3605508aac28caec1ad02be80cf8
Alberto Carvalho <alberto.a.carvalho@gmail.com>
2023-10-05 18:34:22 +0100

    Html - Defining Variables and Constants I

--- Angular/Learning01Book/chp03/Primer/src/main.ts
+++ Angular/Learning01Book/chp03/Primer/src/main.ts
@@ -12,15 +12,6 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
   */
 
-  function myFunction(param: number | string) {
-    if (typeof(param) == "number") {
-        let numberResult = param.toFixed(2);
-        console.log("My result: " + numberResult);
-      } else {
-        let stringResult = param + 100;
-        console.log("My result: " + stringResult);
-      }
-   }
-
-   myFunction(1);
-   myFunction("London");
+  let condition = true;
+  let person = "Bob";
+  const age = 40;
