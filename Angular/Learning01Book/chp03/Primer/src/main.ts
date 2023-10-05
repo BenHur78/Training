b260dd94f2506047af2a87417f07ec374358ef3e
Alberto Carvalho <alberto.a.carvalho@gmail.com>
2023-10-05 18:29:14 +0100

    Htnl - using a type union and expressing ambiguity

--- Angular/Learning01Book/chp03/Primer/src/main.ts
+++ Angular/Learning01Book/chp03/Primer/src/main.ts
@@ -12,15 +12,14 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
   */
 
-  //Typescript requires we to explicitly declare param type to any
-  function myFunction(param: number) {
-    //if (typeof(param) == "number") {
-      let result = param + 100;
+  function myFunction(param: number | string) {
+    if (typeof(param) == "number" || typeof(param) == "string") {
+      let result = (param as any) + 100;
       console.log("My result: " + result);
-    //} else {
-    //  throw ("Expected a number: " + param)
-    //}
+    } else {
+      throw ("Expected a number or a string: " + param)
+    }
    }
 
    myFunction(1);
-   //myFunction("London");
+   myFunction("London");
