6fbb2faebaba35a005b5c3b37b6b8850f070f427
Alberto Carvalho <alberto.a.carvalho@gmail.com>
2023-10-05 18:51:50 +0100

    Html - Using the Null and Nullish Coalescing Operators II

--- Angular/Learning01Book/chp03/Primer/src/main.ts
+++ Angular/Learning01Book/chp03/Primer/src/main.ts
@@ -11,10 +11,12 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
   */
-
   let val1: string | undefined;
   let val2: string | undefined = "London";
+  let val3: number | undefined = 0;
   let coalesced1 = val1 || "fallback value";
   let coalesced2 = val2 || "fallback value";
+  let coalesced3 = val3 || 100;
   console.log(`Result 1: ${coalesced1}`);
   console.log(`Result 2: ${coalesced2}`);
+  console.log(`Result 3: ${coalesced3}`);
