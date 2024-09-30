02eab7c6f5bd3230fe881e6df6608710f2cd1ede
Alberto Carvalho <Alberto.Carvalho@faro.com>
2024-09-30 11:24:28 +0100

    Cplusplus - how to update a project c++/cli with delegate to .Net 6

---
+++ Cplusplus/UpdatingToNet6/DelegateProblem/DelegateProblem/Program.cs
@@ -0,0 +1,6 @@
+﻿using MeshingWrapper;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var result = PcIOManager.Load(@"c:\xpto.txt", null); ;
---
+++ Cplusplus/UpdatingToNet6/DelegateProblemNet48/MeshingWrapper/AssemblyInfo.cpp
@@ -0,0 +1,20 @@
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"MeshingWrapper")];
+[assembly:AssemblyDescriptionAttribute(L"")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"")];
+[assembly:AssemblyProductAttribute(L"MeshingWrapper")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2022")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
+
+[assembly:AssemblyVersionAttribute("1.7.*")];
+
+[assembly:ComVisible(false)];
+
+[assembly:CLSCompliantAttribute(true)];
