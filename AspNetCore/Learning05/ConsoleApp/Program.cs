9161428935baf217f6bb9bf72bd68d7a01d44c65
Alberto Carvalho <Alberto.Carvalho@faro.com>
2024-10-14 11:56:56 +0100

    Learning05 - Debug log is not logged.

---
+++ AspNetCore/Learning05/ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var serviceProvider = new ServiceCollection()
+    .AddLogging(builder => {
+        builder.ClearProviders();
+        builder.AddConsole();
+        builder.AddDebug();
+    })
+    .BuildServiceProvider();
+
+// I've tried this
+//var logger = serviceProvider.GetService<ILogger<Program>>();
+
+// And this
+var logger = serviceProvider.GetService<ILoggerFactory>().CreateLogger<Program>();
+
+logger.LogDebug("hello world");       // <-- This DOESN'T work
+logger.LogInformation("something");   // <-- This DOES work
---
+++ AspNetCore/Learning05/README.md
@@ -0,0 +1,2 @@
+# Introduction 
+This is about to hosting an asp.net web api in a console app.
