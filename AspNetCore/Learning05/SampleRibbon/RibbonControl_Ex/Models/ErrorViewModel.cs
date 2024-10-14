244d4ef90cac758d34c7558e1da9268e4d4a09ce
Alberto Carvalho <Alberto.Carvalho@faro.com>
2024-10-14 15:41:35 +0100

    Learning05 - Using a WebApplication in a desktop app.

---
+++ AspNetCore/Learning05/SampleRibbon/RibbonControl_Ex/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RibbonControl_Ex
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- AspNetCore/Learning05/SampleRibbon/RibbonControl_Ex/WebApiInitializer.cs
+++ AspNetCore/Learning05/SampleRibbon/RibbonControl_Ex/WebApiInitializer.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -21,25 +22,58 @@ namespace RibbonControl_Ex
 
         public void InitWebApi()
         {
-            //var builder1 = WebApplication.CreateBuilder();
+            var builder = WebApplication.CreateBuilder();
             //builder.Environment.EnvironmentName = "Development";
             //builder.Logging.ClearProviders(); //did not worked
             //builder.Logging.AddConsole();
 
 
-            var builder = Host.CreateDefaultBuilder().ConfigureWebHostDefaults(webBuilder =>
+            //var builder = Host.CreateDefaultBuilder().ConfigureWebHostDefaults(webBuilder =>
+            //{
+            //    webBuilder.ConfigureKestrel(serverOptions =>
+            //    {
+            //        serverOptions.ListenLocalhost(18081, o => o.Protocols =
+            //            HttpProtocols.Http1AndHttp2);
+            //    });
+
+            //    webBuilder.UseStartup<Startup>();
+
+            //});
+
+            builder.WebHost.ConfigureKestrel(serverOptions =>
             {
-                webBuilder.ConfigureKestrel(serverOptions =>
-                {
-                    serverOptions.ListenLocalhost(18081, o => o.Protocols =
-                        HttpProtocols.Http1AndHttp2);
-                });
-
-                webBuilder.UseStartup<Startup>();
-
+                serverOptions.ListenLocalhost(18081, o => o.Protocols =
+                    HttpProtocols.Http1AndHttp2);
             });
 
+            builder.Services.AddLogging((bd) =>
+            {
+                bd.SetMinimumLevel(LogLevel.Trace);
+                //bd.ClearProviders();
+                bd.AddDebug();
+                bd.AddFilter("Microsoft.Hosting.Lifetime", LogLevel.Trace);
+                bd.AddFilter("Default", LogLevel.Trace);
+            });
+
+            builder.Services.AddControllers();
+
             var app = builder.Build();
+
+            app.UseRouting();
+
+            //app.MapControllers();
+
+            //app.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
             _webApiHost = app;
 
             try
