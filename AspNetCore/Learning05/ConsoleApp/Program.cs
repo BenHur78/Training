ca32c2233849a8596227df331e9a5cce8176c98c
Alberto Carvalho <Alberto.Carvalho@faro.com>
2024-10-14 14:49:24 +0100

    Learning05 - Debug is logged. We have file logger provider.

--- AspNetCore/Learning05/ConsoleApp/Program.cs
+++ AspNetCore/Learning05/ConsoleApp/Program.cs
@@ -1,22 +1,35 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using ConsoleApp;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
 var serviceProvider = new ServiceCollection()
-    .AddLogging(builder => {
-        builder.ClearProviders();
-        builder.AddConsole();
-        builder.AddDebug();
+    .AddLogging(builder =>
+    {
+        builder.SetMinimumLevel(LogLevel.Trace);
+        //builder.ClearProviders();
+        //builder.AddConsole();
+        //builder.AddDebug();
+        builder.AddProvider(new FileLoggerProvider(@"C:\Dev\Alberto\Training\AspNetCore\Learning05"));
     })
     .BuildServiceProvider();
 
+// This works
+//ServiceProvider serviceProvider = new ServiceCollection()
+//    .AddLogging((loggingBuilder) => loggingBuilder
+//        .SetMinimumLevel(LogLevel.Trace)
+//        .AddConsole()
+//    )
+//    .BuildServiceProvider();
+
 // I've tried this
-//var logger = serviceProvider.GetService<ILogger<Program>>();
+var logger = serviceProvider.GetService<ILogger<Program>>();
 
 // And this
-var logger = serviceProvider.GetService<ILoggerFactory>().CreateLogger<Program>();
+//var logger = serviceProvider.GetService<ILoggerFactory>().CreateLogger<Program>();
 
-logger.LogDebug("hello world");       // <-- This DOESN'T work
-logger.LogInformation("something");   // <-- This DOES work
+logger.LogDebug("Debug world");       // <-- This DOESN'T work
+logger.LogInformation("Information world");   // <-- This DOES work
+logger.LogTrace("Trace world");
