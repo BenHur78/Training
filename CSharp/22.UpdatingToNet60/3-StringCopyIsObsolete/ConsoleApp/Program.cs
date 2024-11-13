80c4161c3c273b27b23665099821538b76062059
Alberto Carvalho <Alberto.Carvalho@faro.com>
2024-11-13 15:01:34 +0000

    UpdatedToNet6 - string.Copy is obsolete.

---
+++ CSharp/22.UpdatingToNet60/3-StringCopyIsObsolete/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // See https://github.com/dotnet/runtime/issues/27515 for more information
+            string str = " abc ";
+            string sTrimmed = str.Trim();
+            Console.WriteLine($"str start={str}=end");
+            Console.WriteLine($"str start={sTrimmed}=end");
+        }
+
+    }
+}
