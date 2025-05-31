b1b2f09c65910a1aa2fd1be5305c9cc3a5b9e58f
Alberto Carvalho <Alberto.Carvalho@faro.com>
2025-05-31 18:05:56 +0100

    Learning - C# Concurrency Parallel Task

---
+++ CSharp/3.Concurrency/3.2Parallel/ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+
+Parallel.For(0, 10, i => A(i));
+
+[MethodImpl(MethodImplOptions.NoInlining)]
+static void A(int i) => B(i);
+
+[MethodImpl(MethodImplOptions.NoInlining)]
+static void B(int i) => C(i);
+
+[MethodImpl(MethodImplOptions.NoInlining)]
+static void C(int index)
+{
+    ThreadPool.GetMaxThreads(out int maxWorkerThreads, out int maxIoThreads);
+    ThreadPool.GetMinThreads(out int minWorkerThreads, out int minIoThreads);
+    ThreadPool.GetAvailableThreads(out int availWorkerThreads, out int availIoThreads);
+    int activeWorkerThreads = maxWorkerThreads - availWorkerThreads;
+    int activeIoWorkerThreads = maxWorkerThreads - availIoThreads;
+    Console.WriteLine($"Index {index} - activeWorkerThreads = {activeWorkerThreads} availWorkerThreads = {availWorkerThreads} activeIoWorkerThreads = {activeIoWorkerThreads} availIoThreads = {availIoThreads} maxWorkerThreads = {maxWorkerThreads} maxIoThreads = {maxIoThreads} ");
+
+    Thread.Sleep(1_000_000);
+}
+
+
