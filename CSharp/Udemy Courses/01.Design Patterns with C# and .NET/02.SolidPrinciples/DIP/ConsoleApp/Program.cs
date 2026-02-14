ab6228465a3f87a66bdc77dda747b81d7b72add4
Alberto Carvalho <Alberto.Carvalho@faro.com>
2026-02-14 13:51:14 +0000

    Design Patterns: Defining a gineology between people, relationshipgs

--- CSharp/Udemy Courses/01.Design Patterns with C# and .NET/02.SolidPrinciples/DIP/ConsoleApp/Program.cs
+++ CSharp/Udemy Courses/01.Design Patterns with C# and .NET/02.SolidPrinciples/DIP/ConsoleApp/Program.cs
@@ -1,5 +1,28 @@
 ﻿namespace DesignPatterns
 {
+    enum Relationship
+    {
+        Parent,
+        Child,
+        Sibling
+    }
+
+    class Person
+    {
+        public string Name;
+    }
+
+    // low-level module
+    class Relationshipts
+    {
+        private List<(Person, Relationship, Person)> relations = new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            relations.Add((parent, Relationship.Parent, child));
+            relations.Add((child, Relationship.Child, parent));
+        }
+
     public class Demo
     {
         static void Main(string[] args)
--- CSharp/Udemy Courses/01.Design Patterns with C# and .NET/02.SolidPrinciples/DIP/README.md
+++ CSharp/Udemy Courses/01.Design Patterns with C# and .NET/02.SolidPrinciples/DIP/README.md
@@ -1,5 +1,6 @@
 # Steps
-- 
-- 
+- Creating a system with high levels parts and low levels parts
+- We will model a gineology, relationships betweem different people
+- Imagine a system where we perform queries to a gineology database
 # What we learned here
 - 
