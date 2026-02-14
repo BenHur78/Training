7717f8fb4323e176ad6d663404db8487bf89f913
Alberto Carvalho <Alberto.Carvalho@faro.com>
2026-02-14 11:01:45 +0000

    Design Patterns: A fat interface that does everything

--- CSharp/Udemy Courses/01.Design Patterns with C# and .NET/02.SolidPrinciples/ISP/ConsoleApp/Program.cs
+++ CSharp/Udemy Courses/01.Design Patterns with C# and .NET/02.SolidPrinciples/ISP/ConsoleApp/Program.cs
@@ -1,5 +1,49 @@
 ﻿namespace DesignPatterns
 {
+    public class Document
+    {
+
+    }
+
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+
+    public class MultiFunctionMachine : IMachine
+    {
+        public void Print(Document d)
+        {
+            Console.WriteLine("Printing document...");
+        }
+        public void Scan(Document d)
+        {
+            Console.WriteLine("Scanning document...");
+        }
+        public void Fax(Document d)
+        {
+            Console.WriteLine("Faxing document...");
+        }
+    }
+
+    public class OldFashionedPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            Console.WriteLine("Printing document...");
+        }
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public class Demo
     {
         static void Main(string[] args)
--- CSharp/Udemy Courses/01.Design Patterns with C# and .NET/02.SolidPrinciples/ISP/README.md
+++ CSharp/Udemy Courses/01.Design Patterns with C# and .NET/02.SolidPrinciples/ISP/README.md
@@ -1,5 +1,6 @@
 # Steps
-- 
-- 
+- A fat interface that does multiple things.
+- Now we have a problem when we have a old printer that only supports printing and not scanning and other features.
+
 # What we learned here
 - 
