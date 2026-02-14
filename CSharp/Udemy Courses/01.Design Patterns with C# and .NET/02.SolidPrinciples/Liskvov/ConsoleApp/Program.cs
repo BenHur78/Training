ad933fe6ef68e11e8af707186eacb49840e8b458
Alberto Carvalho <Alberto.Carvalho@faro.com>
2026-02-14 09:59:41 +0000

    Design Patterns: Creating Rectangle class.

--- CSharp/Udemy Courses/01.Design Patterns with C# and .NET/02.SolidPrinciples/Liskvov/ConsoleApp/Program.cs
+++ CSharp/Udemy Courses/01.Design Patterns with C# and .NET/02.SolidPrinciples/Liskvov/ConsoleApp/Program.cs
@@ -1,9 +1,35 @@
 ﻿namespace DesignPatterns
 {
+    public class Rectangle
+    {
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+
+    public class Functions
+    {
+        public static int Area(Rectangle r) => r.Width * r.Height;
+    }
+
     public class Demo
     {
         static void Main(string[] args)
         {
+            var rc = new Rectangle(2, 3);
+            Console.WriteLine($"{rc} has area {Functions.Area(rc)}");
         }
     }
 
