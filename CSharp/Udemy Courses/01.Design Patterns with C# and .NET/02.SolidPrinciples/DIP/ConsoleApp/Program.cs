6d8a31a2ede4f796fc6d7191478c1610faa96067
Alberto Carvalho <Alberto.Carvalho@faro.com>
2026-02-14 14:17:11 +0000

    Design Patterns: How relationships can change? By inverting the direction of the dependency.

--- CSharp/Udemy Courses/01.Design Patterns with C# and .NET/02.SolidPrinciples/DIP/ConsoleApp/Program.cs
+++ CSharp/Udemy Courses/01.Design Patterns with C# and .NET/02.SolidPrinciples/DIP/ConsoleApp/Program.cs
@@ -12,9 +12,15 @@
         public string Name;
     }
 
-    // low-level module
-    class Relationshipts
+    interface IRelationshipBrowser
     {
+        IEnumerable<Person> FindAllChildrenOf(string name);
+    }
+
+    // low-level module
+    class Relationshipts : IRelationshipBrowser
+    {
+        //the high level module depends on how we structure data
         private List<(Person, Relationship, Person)> relations = new List<(Person, Relationship, Person)>();
 
         public void AddParentAndChild(Person parent, Person child)
@@ -23,17 +29,32 @@
             relations.Add((child, Relationship.Child, parent));
         }
 
-        public List<(Person, Relationship, Person)> Relations => relations; //bad, this is a private thing being exposed.
+        public IEnumerable<Person> FindAllChildrenOf(string name)
+        {
+            return relations.Where(x => x.Item1.Name == name && x.Item2 == Relationship.Parent).Select(r => r.Item3);
+        }
+
+        
+
+        //public List<(Person, Relationship, Person)> Relations => relations; //bad, this is a private thing being exposed.
     }
 
     class Research //the high module
     {
-        public Research(Relationshipts relationshipts)
+        //public Research(Relationshipts relationships)
+        //{
+        //    //var relations = relationshipts.Relations; // this is bad, we are accessing a low level detail, a datastore.
+        //    //foreach (var r in relations.Where(x => x.Item1.Name == "John" && x.Item2 == Relationship.Parent))
+        //    //{
+        //    //    Console.WriteLine($"John has a child called {r.Item3.Name}");
+        //    //}
+        //}
+
+        public Research(IRelationshipBrowser browser)
         {
-            var relations = relationshipts.Relations; // this is bad, we are accessing a low level detail, a datastore.
-            foreach (var r in relations.Where(x => x.Item1.Name == "John" && x.Item2 == Relationship.Parent))
+            foreach (var p in browser.FindAllChildrenOf("John"))
             {
-                Console.WriteLine($"John has a child called {r.Item3.Name}");
+                Console.WriteLine($"John has a child called {p.Name}");
             }
         }
 
--- CSharp/Udemy Courses/01.Design Patterns with C# and .NET/02.SolidPrinciples/DIP/README.md
+++ CSharp/Udemy Courses/01.Design Patterns with C# and .NET/02.SolidPrinciples/DIP/README.md
@@ -3,6 +3,11 @@
 - We will model a gineology, relationships betweem different people
 - Imagine a system where we perform queries to a gineology database
 - Because the high level depends on Relationships, it means it cannot change without affecting the high level module.
+- How relationships can change?
+
 
 # What we learned here
-- 
+- The principle teach us how to invert the direction of the dependencies. Before the high level module depend upon the low level module.
+- Now we can create an abstraction that the low level module will depend on. This way the high level module will not depend
+on low level things.
+- Now the low level module can change without impacting the high level module.
